5b508e6ef5b4b3a9633171bca7f739e33fff98f6
Sonal Wijenayaka <sonalwijenayaka@gmail.com>
2025-08-07 14:49:32 +0530

    .

--- src/auth/auth-request.interface.ts
+++
@@ -1,7 +0,0 @@
-// src/auth/interfaces/auth-request.interface.ts
-import { Request } from 'express';
-import { User } from '../users/entities/user.entity'; // Fixed relative path
-
-export interface AuthRequest extends Request {
-  user: User;
-}
--- src/users/enums/role.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum Role {
-  Admin = 'Admin',
-  Pharmacist = 'Pharmacist',
-  Cashier = 'Cashier',
-  Customer = 'Customer',
-  Supplier = 'Supplier'
-}
